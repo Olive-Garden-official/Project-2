ce5a5b0c6700e03c147b6de1c54433a1fdb48885
MagusConjurer <cameronisadavis@gmail.com>
2020-06-11 20:24:32 -0600

    Updated ingredient and recipe models

--- models/ingredients.js
+++ models/ingredients.js
@@ -1,7 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
   var Ingredients = sequelize.define("Ingredients", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+    name: DataTypes.STRING
   });
   return Ingredients;  
 };
