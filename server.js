74c391d507bccafc61c9fc03e668a281219d83b4
Jakenbake20 <jacob_wheat@yahoo.com>
2020-06-11 19:07:23 -0600

    re adding connection/db

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var db = require("./models");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+
 //Establising connection to DB
 var connection = mysql.createConnection({
   host: "localhost",
