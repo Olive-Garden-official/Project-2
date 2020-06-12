d7f0df16d098bfaaac06f2aa72a0090eafa7ed3b
MagusConjurer <cameronisadavis@gmail.com>
2020-06-11 21:33:43 -0600

    Started addRecipe with base variable setup

---
+++ public/js/addRecipe.js
@@ -0,0 +1,11 @@
+var recipeName = $(); // String name
+var recipeInstructions = $() //Text string
+var recipeIngredients = $() // JSON.stringify the object
+var recipeImage = $() // Link to image, set default
+var recipeCategory = $() // String category
+var recipeAuthor = $() // String name, default to Unknown
+
+var API = {
+
+};
+
