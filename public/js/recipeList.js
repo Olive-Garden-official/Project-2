77401941177a3c34e0faa1cd44dff134eaac24e1
Jakenbake20 <jacob_wheat@yahoo.com>
2020-06-13 11:36:01 -0600

    added basic code that can be later filled out with selectors from other pages.

--- public/js/recipeList.js
+++ public/js/recipeList.js
@@ -1,2 +1,29 @@
 //create a list for adding a recipes to a list that will be stored locally
+
+
+
 //users should be able to choose desired recipe and add it to a list to see later.
+
+// make a "clear" button to clear list of recipe items
+
+$('#clearbuttonname').click(function(){
+    $(':input','#formID')
+    .not(':button, :submit, :reset, :hidden')
+    .val('')
+    .removeAttr('checked')
+    .removeAttr('selected');
+
+    $("#formID #multiple").empty();
+});
+
+
+// create local storage for storing and retrieving recipe choices
+
+
+
+
+
+
+localStorage.setItem("");
+
+document.getElementById("").innerHTML = localStorage.getItem("");
