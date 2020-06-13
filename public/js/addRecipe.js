9bffbfef6fe620dce576395bbc897a71525eb075
MagusConjurer <cameronisadavis@gmail.com>
2020-06-13 11:12:17 -0600

    Temp fix for htmlRoute. Fixed image and size of 404 image.

--- public/js/addRecipe.js
+++ public/js/addRecipe.js
@@ -1,11 +1,31 @@
-var recipeName = $(); // String name
-var recipeInstructions = $() //Text string
-var recipeIngredients = $() // JSON.stringify the object
-var recipeImage = $() // Link to image, set default
-var recipeCategory = $() // String category
-var recipeAuthor = $() // String name, default to Unknown
+$(document).ready(function(){
 
-var API = {
+  var recipeName = $(); // String name
+  var recipeInstructions = $(); //Text string
+  var recipeIngredients = $(); // JSON.stringify the object
+  var recipeImage = $(); // Link to image, set default
+  var recipeCategory = $(); // String category
+  var recipeAuthor = $(); // String name, default to Unknown
+  
+  var submitRecipe = function(event){
+    event.preventDefault();
 
-};
+
+
+
+  };
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
 
