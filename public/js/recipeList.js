af23ad422902c9a80c2848a4dc3e4fa653c3eca2
Jakenbake20 <jacob_wheat@yahoo.com>
2020-06-18 20:22:26 -0600

    added some styling to recipe page, added jquery cdn to recipe page, made sure the clear and save buttons worked, made sure handlebars npm was installed which it was

--- public/js/recipeList.js
+++ public/js/recipeList.js
@@ -2,38 +2,41 @@
 
 // const ingredients = require("../../models/ingredients");
 
-var items = [''];
-var rList = $('ul.mylist');
-items.map( (item,i ) => {
-      var li = $('<li/>')
-        .addClass('ui-menu-item')
-        .attr('role', 'menuitem')
-        .appendTo(rList);
-      $('<a class="ui-all">'+ i + ': ' + item.name + '<a/>')
-        .appendTo(li);
-    });
+$(document).ready(function(){
+// var items = [''];
+// var rList = $('ul.mylist');
+// items.map( (item,i ) => {
+//       var li = $('<li/>')
+//         .addClass('ui-menu-item')
+//         .attr('role', 'menuitem')
+//         .appendTo(rList);
+//       $('<a class="ui-all">'+ i + ': ' + item.name + '<a/>')
+//         .appendTo(li);
+//     });
 // ".mylist" will be changed to match the class name of the ul list created in the html and classes can be changed to match that of materialize.
 //---------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //users should be able to choose and create desired recipe and add it to a list to see later.
 
-var recipeVariable = $("#recipe-input").val().trim();
-var ingredientVariable = $("#ingredient-input").val().trim();
-var measurement = $("#measurement-input").val().trim();
-var comment = $("#comment-input").val().trim();
+// var recipeVariable = $("#recipe-input").val().trim();
+// var ingredientVariable = $("#ingredient-input").val().trim();
+// var measurement = $("#measurement-input").val().trim();
+// var comment = $("#comment-input").val().trim();
 
 
-$("#recipe-display").text(recipeVariable);
-$("#ingredient-display").text(ingredientVariable);
-$("#measurement-display").text(measurement);
-$("#comment-display").text(comment);
+// $("#recipe-display").text(recipeVariable);
+// $("#ingredient-display").text(ingredientVariable);
+// $("#measurement-display").text(measurement);
+// $("#comment-display").text(comment);
 
 //something like this maybe
-$('<ul />').append($('ul li.chosenRecipe').clone()).appendTo(document.body);
+
+// $('<ul />').append($('ul li.chosenRecipe').clone()).appendTo(document.body);
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // make a "clear" button to clear list of recipe items
+
 $('#clear').click(function(){
     $(':input','#recipeHolder')
     .not(':button, :submit, :reset, :hidden')
@@ -42,6 +45,8 @@ $('#clear').click(function(){
     .removeAttr('selected');
 
     $("#recipeHolder").empty();
+
+    console.log("item cleared");
 });
 
 //can clear local storage if we want a button for that as well.
@@ -66,7 +71,9 @@ $('#clrRec').click(function(){
   var clear = $('#clrRec').value;
   localStorage.clear('server', clear);
 
-  $('#save').value = localStorage.getItem('server');
+  $('#clrRec').value = localStorage.clear('server');
+
+  console.log("localstorage cleared");
 
 });
 
@@ -78,3 +85,6 @@ $('#clrRec').click(function(){
 
 // document.getElementById("").innerHTML = localStorage.getItem("");
 
+
+
+});
